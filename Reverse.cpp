34346e969165b762dcd9b94691a0f76e8ed21f4c
Sifat Ullah <usifat59@gmail.com>
2024-09-10 00:06:37 +0600

    forverse

---
+++ Reverse.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insertNode(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void reverseList() {
+        Node* prev = nullptr;
+        Node* current = head;
+        Node* next = nullptr;
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    void displayList() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    ~LinkedList() {
+        Node* temp;
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    LinkedList list;
+    int n, value;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cout << "Enter value " << i + 1 << ": ";
+        cin >> value;
+        list.insertNode(value);
+    }
+
+    cout << "Original Linked List:\n";
+    list.displayList();
+
+    list.reverseList();
+
+    cout << "Reversed Linked List:\n";
+    list.displayList();
+
+    return 0;
+}
